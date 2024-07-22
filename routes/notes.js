29d00ebc170cbde52130e6b05332d47ecc98c6da
emi-osogami <85gold85@gmail.com>
2024-07-23 05:39:43 +0900

    add change

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "********";
+const uri = "mongodb+srv://oso:mlea3939@cluster0.z2frb8p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
